9735722ee99aa40c8a347735c4bc19eaedd0c598
jin-hyeok08 <auxury@naver.com>
2021-12-20 16:03:51 +0900

    catchall url

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => res.render("home.pug"));
+app.get("/*", (req, res) => res.redirect("/")); //catchall url,, 이외의 url은 모두 홈으로
 
 const handleListen = () => {
   console.log(`Listening on port http://localhost:${PORT}`);
