863357af8cf4c0dfeabb6f7d767fd54d11025e8d
jin-hyeok08 <auxury@naver.com>
2021-12-23 18:37:03 +0900

    real time msg with nickname

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,14 @@
 const socket = new WebSocket(`ws://${window.location.host}`); //모바일에서도 가능하도록 주소를 window.location.host로 명시
 //셀렉터
 const msgList = document.querySelector("ul");
-const msgForm = document.querySelector("form");
+const msgForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nick");
+
+//메시지 오브젝트를 stringify..socket.send()로 서버에 보낼때 string으로 보내야 서버 언어 관계 없이 작동
+const makeMessage = (type, payload) => {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+};
 
 //백엔드에서 connection socket한 것을 프론트에서 다음과 같이 받음
 socket.addEventListener("open", () => {
@@ -11,6 +18,9 @@ socket.addEventListener("open", () => {
 
 socket.addEventListener("message", (message) => {
   console.log("Message from the Server:", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  msgList.append(li);
 });
 
 socket.addEventListener("close", () => {
@@ -21,12 +31,19 @@ socket.addEventListener("close", () => {
 //   socket.send("hello, this is Browser");
 // }, 5000);
 
-const handleSubmit = (event) => {
+const handleMsgSubmit = (event) => {
   event.preventDefault();
   const input = msgForm.querySelector("input");
   //input의 value를 socket을 통해 백엔드로 보냄
-  socket.send(input.value);
+  socket.send(makeMessage("newMessage", input.value));
   input.value = ""; //input값 초기화
 };
 
-msgForm.addEventListener("submit", handleSubmit);
+const handleNickSubmit = (event) => {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+};
+
+msgForm.addEventListener("submit", handleMsgSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
